4ace5dc37ac9ecb1661bac2c1c03659d8ca5f333
Dominik Schürmann <dominik@dominikschuermann.de>
2015-09-18 05:18:07 +0200

    Initial commit

---
+++ app/src/main/java/org/sufficientlysecure/keychain/gm/GmAccessibilityService.java
@@ -0,0 +1,181 @@
+/*
+ * Copyright (C) 2015 Dominik Schürmann <dominik@dominikschuermann.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.sufficientlysecure.keychain.gm;
+
+import android.accessibilityservice.AccessibilityService;
+import android.util.Log;
+import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+
+import java.util.ArrayList;
+
+public class GmAccessibilityService extends AccessibilityService {
+
+    private static final String LOG_TAG = "Keychain_gm";
+
+    private static final String WEB_VIEW_CLASS_NAME = "android.webkit.WebView";
+    private static final String VIEW_CLASS_NAME = "android.view.View";
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onServiceConnected() {
+    }
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+
+        if (!(AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED == event.getEventType())) {
+            return;
+        }
+        Log.d(LOG_TAG, "TYPE_WINDOW_CONTENT_CHANGED");
+
+
+        // This AccessibilityNodeInfo represents the view that fired the
+        // AccessibilityEvent. The following code will use it to traverse the
+        // view hierarchy, using this node as a starting point.
+        //
+        // NOTE: Every method that returns an AccessibilityNodeInfo may return null,
+        // because the explored window is in another process and the
+        // corresponding View might be gone by the time your request reaches the
+        // view hierarchy.
+//        AccessibilityNodeInfo source = event.getSource();
+
+        // TODO: start with "com.google.android.gm:id/conversation" ?
+        AccessibilityNodeInfo source = getRootInActiveWindow();
+        if (source == null) {
+            return;
+        }
+        Log.i(LOG_TAG, "ACC::onAccessibilityEvent: nodeInfo=" + source);
+
+        ArrayList<AccessibilityNodeInfo> pgpNodes = new ArrayList<>();
+        AccessibilityNodeInfo rowNode = getListItemNodeInfo(source, pgpNodes);
+        if (rowNode == null) {
+            return;
+        }
+
+
+        // TODO
+        // - try out if this webview has AccessibilityRecord
+        // - check if
+
+//        // The custom ListView added extra context to the event by adding an
+//        // AccessibilityRecord to it. Extract that from the event and read it.
+//        final int records = event.getRecordCount();
+//        for (int i = 0; i < records; i++) {
+//            AccessibilityRecord record = event.getRecord(i);
+//            CharSequence contentDescription = record.getContentDescription();
+//            if (!TextUtils.isEmpty(contentDescription)) {
+//                utterance.append(SEPARATOR);
+//                utterance.append(contentDescription);
+//            }
+//        }
+
+
+//        AccessibilityNodeInfo root = getRootInActiveWindow();
+//        ArrayDeque<AccessibilityNodeInfo> nodeQueue
+//                = new ArrayDeque<AccessibilityNodeInfo>();
+//        nodeQueue.add(root);
+//
+//        AccessibilityNodeInfo target = null;
+//
+//        for (AccessibilityNodeInfo node = nodeQueue.pollFirst(); node != null; node = nodeQueue.pollFirst()) {
+//            Rect currRect = new Rect();
+//
+//            node.getBoundsInScreen(currRect);
+////            if(!currRect.contains(eventX, eventY)) {
+////                node.recycle();
+////                continue;
+////            }
+//
+//            if (target == null) {
+//                target = node;
+//                continue;
+//            }
+//
+//            Rect targetRect = new Rect();
+//            target.getBoundsInScreen(targetRect);
+//            if (currRect.width() * currRect.height() <
+//                    targetRect.width() * targetRect.height()) {
+//                target.recycle();
+//                target = node;
+//            }
+//        }
+    }
+
+    private AccessibilityNodeInfo getListItemNodeInfo(AccessibilityNodeInfo parent,
+                                                      ArrayList<AccessibilityNodeInfo> pgpNodes) {
+
+        for (int i = 0; i < parent.getChildCount(); i++) {
+            AccessibilityNodeInfo currentChild = parent.getChild(i);
+            Log.d(LOG_TAG, "currentChild.getClassName() " + currentChild.getClassName());
+
+            /*
+             WebView
+             |- WebView
+                |- View
+                |- View
+                |- View
+             */
+            if (WEB_VIEW_CLASS_NAME.equals(parent.getClassName())
+                    && VIEW_CLASS_NAME.equals(currentChild.getClassName())
+                    && currentChild.getContentDescription() != null) {
+
+                Log.d(LOG_TAG, "MATCHED");
+                // -----BEGIN PGP MESSAGE-----
+
+
+                CharSequence content = currentChild.getContentDescription();
+                for (String line : ((String) content).split("\n")) {
+                    Log.d(LOG_TAG, line);
+                }
+
+                pgpNodes.add(currentChild);
+
+            } else {
+                // recursive
+                AccessibilityNodeInfo nd = getListItemNodeInfo(currentChild, pgpNodes);
+                if (nd == null) {
+                    continue;
+                }
+            }
+
+            // NOTE: Recycle the infos.
+            currentChild.recycle();
+        }
+
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onInterrupt() {
+        /* do nothing */
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
---
+++ app/src/main/java/org/sufficientlysecure/keychain/gm/MainActivity.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2015 Dominik Schürmann <dominik@dominikschuermann.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.sufficientlysecure.keychain.gm;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final Intent sSettingsIntent =
+            new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        Button button = (Button) findViewById(R.id.main_activity_accessibility_settings_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(sSettingsIntent);
+            }
+        });
+    }
+
+}
